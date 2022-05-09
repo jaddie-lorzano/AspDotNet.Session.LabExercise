6be5c959d99fdd2e2ec5149e5d57e20ddb09b006
jaddielorzano-pointwest <jaddie.lorzano@pointwest.com.ph>
2022-05-10 03:27:45 +0800

    Create Unit of Work

---
+++ ToyUniverse.Data/Data/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToyUniverse.Data.Repositories;
+
+namespace ToyUniverse.Data.Data
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        public IToyRepository ToyRepository { get; }
+        public IShoppingCartRepository ShoppingCartRepository { get; set; }
+    }
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private ToyUniverseContext context;
+        public IToyRepository ToyRepository { get; private set; }
+        public IShoppingCartRepository ShoppingCartRepository { get; set; }
+
+        public UnitOfWork(ToyUniverseContext context)
+        {
+            this.context = context;
+            this.ToyRepository = new ToyRepository(context);
+            this.ShoppingCartRepository = new ShoppingCartRepository(context);
+        }
+
+        public async Task CommitAsync()
+        {
+            await this.context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            this.context.Dispose();
+        }
+    }
+}
