d31e9188ed9a023be7ffd9ed2ede12b16a56a8d3
jaddielorzano-pointwest <jaddie.lorzano@pointwest.com.ph>
2022-05-09 16:18:24 +0800

    Add Migration Skills Data Seeding

---
+++ WorkSchedule.Data/Migrations/20220509080816_SkillsDataSeeding.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WorkSchedule.Data.Migrations
+{
+    public partial class SkillsDataSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql
+                (
+                    @"
+                    INSERT INTO Skills 
+                        (
+                            Description
+                        )
+                    VALUES 
+                        (
+                            'Communication Skill'
+                        ),
+                        (
+                            'Coding Skill'
+                        ),
+                        (
+                            'Office Application Skill'
+                        ),
+                        (
+                            'Data Entry Skill'
+                        ),
+                        (
+                            'Leadership Skill'
+                        ),
+                        (
+                            'Bookkeeping Skill'
+                        ),
+                        (
+                            'Accounting Skill'
+                        ),
+                        (
+                            'Presentation Skill'
+                        ),
+                        (
+                            'Public Speaking Skill'
+                        ),
+                        (
+                            'Teaching Skill'
+                        ),
+                        (
+                            'Project Management Skill'
+                        ),
+                        (
+                            'Human Resource Skill'
+                        ),
+                        (
+                            'Training Skill'
+                        ),
+                        (
+                            'Analytical Skill'
+                        ),
+                        (
+                            'Computational Skill'
+                        ),
+                        (
+                            'Engineering Skill'
+                        ),
+                        (
+                            'Interpersonal Skill'
+                        ),
+                        (
+                            'Management Skill'
+                        )
+                    "
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
