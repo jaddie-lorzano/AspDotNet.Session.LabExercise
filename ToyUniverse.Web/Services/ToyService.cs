83519150abcf88330c62fffed9741690e0955bf7
jaddielorzano-pointwest <jaddie.lorzano@pointwest.com.ph>
2022-05-10 03:32:49 +0800

    Create Services

---
+++ ToyUniverse.Web/Services/ToyService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using ToyUniverse.Data.Data;
+using ToyUniverse.Data.Models;
+using ToyUniverse.Data.Repositories;
+using ToyUniverse.Web.Models;
+
+namespace ToyUniverse.Web.Services
+{
+    public interface IToyService
+    {
+        public PageResult<Toy> GetToyPage(int currentPage, int pageSize);
+    }
+
+    public class ToyService : GenericService, IToyService
+    {
+        private IUnitOfWork unitOfWork;
+
+        public ToyService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public PageResult<Toy> GetToyPage(int currentPage, int pageSize)
+        {
+            return GetPage<Toy>(unitOfWork.ToyRepository.Context.Toys, currentPage, pageSize);
+        }
+    }
+}
