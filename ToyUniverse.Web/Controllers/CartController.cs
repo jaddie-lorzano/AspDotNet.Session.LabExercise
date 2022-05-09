71fa8de42819c68b74dc13677dac9dcea5d8aba8
jaddielorzano-pointwest <jaddie.lorzano@pointwest.com.ph>
2022-05-10 03:29:36 +0800

    Create Cart Controller

---
+++ ToyUniverse.Web/Controllers/CartController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToyUniverse.Data.Data;
+using ToyUniverse.Data.Models;
+using ToyUniverse.Web.Extensions;
+
+namespace ToyUniverse.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public ActionResult Index()
+        {
+            if (HttpContext.Session.Get("cart") == null)
+            {
+                List<ShoppingCart> cart = new List<ShoppingCart>();
+
+                HttpContext.Session.SetObject("cart", cart);
+            }
+            return View();
+        }
+
+        public ActionResult Add(string id)
+        {
+            if (HttpContext.Session.GetObject<List<ShoppingCart>>("cart") == null)
+            {
+                List<ShoppingCart> cart = new List<ShoppingCart>();
+                ShoppingCart item = new ShoppingCart();
+                item.CToy = unitOfWork.ToyRepository.FindByPrimaryKey(id);
+                item.SiQty = 1;
+                cart.Add(item);
+
+                HttpContext.Session.SetObject("cart", cart);
+            }
+            else
+            {
+                List<ShoppingCart> cart = HttpContext.Session.GetObject<List<ShoppingCart>>("cart");
+                int index = isExist(id);
+                if (index != -1)
+                {
+                    cart[index].SiQty++;
+                }
+                else
+                {
+                    ShoppingCart item = new ShoppingCart();
+                    item.CToy = unitOfWork.ToyRepository.FindByPrimaryKey(id);
+                    item.SiQty = 1;
+                    cart.Add(item);
+                }
+                HttpContext.Session.SetObject("cart", cart);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Remove(string id)
+        {
+            List<ShoppingCart> cart = HttpContext.Session.GetObject<List<ShoppingCart>>("cart");
+            int index = isExist(id);
+            cart.RemoveAt(index);
+            HttpContext.Session.SetObject("cart", cart);
+            return RedirectToAction("Index");
+        }
+
+        private int isExist(string id)
+        {
+            List<ShoppingCart> cart = HttpContext.Session.GetObject<List<ShoppingCart>>("cart");
+            for (int i = 0; i < cart.Count; i++)
+            {
+                if (cart[i].CToy.CToyId.Equals(id))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
